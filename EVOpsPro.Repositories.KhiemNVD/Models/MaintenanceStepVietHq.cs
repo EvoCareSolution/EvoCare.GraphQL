5f1a2493ac2b29670c428efc3e514283a30cb19a
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-11-06 14:15:00 +0700

    feat(repositories): add maintenance and work order models

---
+++ EVOpsPro.Repositories.KhiemNVD/Models/MaintenanceStepVietHq.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace EVOpsPro.Repositories.KhiemNVD.Models;
+
+public partial class MaintenanceStepVietHq
+{
+    public int MaintenanceStepVietHqid { get; set; }
+
+    public string StepName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int? EstimatedMinutes { get; set; }
+
+    public bool IsFinalStep { get; set; }
+
+    public int SortOrder { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual ICollection<MaintenanceProcessVietHq> MaintenanceProcessVietHqs { get; set; } = new List<MaintenanceProcessVietHq>();
+}
